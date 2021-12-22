1cfdd79812cc8350533d7dd52c37a1872f612c5f
Dot92 <mbb014@googlemail.com>
2021-12-22 13:28:43 +0100

    master

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 import talib
 from math import pi
-import yfinance as yf
-from flask import Flask, render_template, jsonify
-from bokeh.plotting import figure
-from bokeh.embed import components
 import numpy as np
 import pandas as pd
+from pybit import HTTP, WebSocket
 
 
 
-data = yf.download("BTC-USD", start="2020-01-01", end="2021-05-01")
+session = HTTP("https://api-testnet.bybit.com")
+data = session.query_kline(
+    symbol="BTCUSDT",
+    interval=1,
+    limit=2,
+    from_time=1581231260
+)
 
-morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
-upper, middle, lower = talib.BBANDS(data['Close'], 20, 2, 2)
+#data = yf.download("BTC-USD", start="2020-01-01", end="2021-05-01")
 
-data['BB-Up'] = upper
-data['BB-Low'] = lower
-data['Morning Star'] = morning_star
-data['Engulfing'] = engulfing
-data['Date'] = data.index
+#morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+#engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+#upper, middle, lower = talib.BBANDS(data['Close'], 20, 2, 2)
 
-morning_stars = data[data['Morning Star'] != 0]
+##data['BB-Up'] = upper
+#data['BB-Low'] = lower
+#data['Morning Star'] = morning_star
+#data['Engulfing'] = engulfing
+#data['Date'] = data.index
+
+#morning_stars = data[data['Morning Star'] != 0]
 
 #putting data into Pandas Dataframe
-df = pd.DataFrame(data)[:50]
+df = pd.json_normalize(data)[:50]
 
 print(df)
 
