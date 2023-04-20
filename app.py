205e41f3ade37eb86d139b8c73d0db47b5a5620f
Kev-Soft <mbb014@googlemail.com>
2023-04-20 15:41:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,21 @@ data = session.query_kline(
     from_time=1581231260
 )
 
-#data = yf.download("BTC-USD", start="2020-01-01", end="2021-05-01")
+data = yf.download("BTC-USD", start="2020-01-01", end="2021-05-01")
 
-#morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-#engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
-#upper, middle, lower = talib.BBANDS(data['Close'], 20, 2, 2)
+morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+upper, middle, lower = talib.BBANDS(data['Close'], 20, 2, 2)
 
-##data['BB-Up'] = upper
-#data['BB-Low'] = lower
-#data['Morning Star'] = morning_star
-#data['Engulfing'] = engulfing
-#data['Date'] = data.index
+data['BB-Up'] = upper
+data['BB-Low'] = lower
+data['Morning Star'] = morning_star
+data['Engulfing'] = engulfing
+data['Date'] = data.index
 
-#morning_stars = data[data['Morning Star'] != 0]
+morning_stars = data[data['Morning Star'] != 0]
 
-#putting data into Pandas Dataframe
+putting data into Pandas Dataframe
 df = pd.json_normalize(data)[:50]
 
 print(df)
